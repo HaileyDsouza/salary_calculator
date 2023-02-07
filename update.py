976507e0f76e593ac24ad8bd86be022355e45f27
HaileyDsouza <122242260+HaileyDsouza@users.noreply.github.com>
2023-02-07 13:58:22 -0500

    Update update.py

--- update.py
+++ update.py
@@ -1,71 +1,67 @@
 #calculations
 
 def calculate():
-    hour_tot = float(hours_worked_input.get())
-    wage_tot = float(wage_input.get())
-    bi_weekly = ((hour_tot * wage_tot * 2)*100)/100
+    totalSal = 0
+    x_place = 150
+    bi_weeklyy = []
+    for h in create_Hour_input():
+        for w in create_Wage_input():
+            print(h)
+            hour_tot = float(h.get())
+            wage_tot = float(w.get())
+            bi_weekly = ((hour_tot * wage_tot * 2) * 100) / 100
+            totalSal += bi_weekly
+            bi_weeklyy.append(bi_weekly)
 
-    hour_tot2 = float(hours_worked_input2.get())
-    wage_tot2 = float(wage_input2.get())
-    bi_weekly2 = ((hour_tot2 * wage_tot2 * 2)*100)/100
+    for i in bi_weeklyy:
+        bi_weekly_tot = Label(root, text= i, bg='paleturquoise').place(x= x_place, y=150)
+        totalSal = Label(root, text= totalSal, bg='paleturquoise').place(x=210, y=180)
+        x_place += 60
+        return bi_weekly_tot, totalSal
 
-    hour_tot3 = float(hours_worked_input3.get())
-    wage_tot3 = float(wage_input3.get())
-    bi_weekly3 = ((hour_tot3 * wage_tot3 * 2)*100)/100
-
-    totalSal = bi_weekly + bi_weekly2 + bi_weekly3
-
-    bi_weekly_tot = Label(root, text= bi_weekly, bg='paleturquoise').place(x=150, y=150)
-    bi_weekly_tot2 = Label(root, text= bi_weekly2, bg='paleturquoise').place(x=210, y=150)
-    bi_weekly_tot3 = Label(root, text= bi_weekly3, bg='paleturquoise').place(x=270, y=150)
-    totalSal = Label(root, text= totalSal, bg='paleturquoise').place(x=210, y=180)
-
-
-    return bi_weekly_tot, bi_weekly_tot2, bi_weekly_tot3, totalSal
 
 def clear():
-    hours_worked_input.delete(0,END)
-    wage_input.delete(0,END)
-    hours_worked_input2.delete(0, END)
-    wage_input2.delete(0, END)
-    hours_worked_input3.delete(0, END)
-    wage_input3.delete(0, END)
-    tot = Label(root,width=23, fg='paleturquoise', bg='paleturquoise').place(x=140, y=150)
-    tot_sal = Label(root,width=15, fg='paleturquoise', bg='paleturquoise').place(x=170, y=180)
+    for h in create_Hour_input():
+        for w in create_Wage_input():
+            h.delete(0,END)
+            w.delete(0,END)
+
+    tot = Label(root,width=23, fg='paleturquoise', bg='paleturquoise')
+    tot.place(x=140, y=150)
+    tot_sal = Label(root,width=15, fg='paleturquoise', bg='paleturquoise')
+    tot_sal.place(x=170, y=180)
     return tot, tot_sal
 
-def create_Wage_input()
+def create_Wage_input():
     wage_inputt = []
     x_place = 130
-    for i in range(2):
+    for i in range(3):
         wage = Entry(root, width=10)
         wage.place(x=x_place,y=110)
         x_place += 60
         wage_inputt.append(wage)
     return wage_inputt
 
-def create_Hour_input()
+def create_Hour_input():
     hour_inputt = []
     x_place = 130
-    for i in range(2):
+    for i in range(3):
         hour = Entry(root, width=10)
         hour.place(x=x_place,y=70)
         x_place += 60
-        hour_inputt.append(wage)
+        hour_inputt.append(hour)
     return hour_inputt
 
-def create_Job_label()
+def create_Job_label():
     x_place = 140
     jobs = ["1st Job", "2nd Job", "3rd Job"]
     final = []
-    for i in range(2):
+    for i in range(3):
         job_label = Label(root, text= jobs[i], bg='paleturquoise')
         job_label.place(x = x_place, y = 40)
         x_place += 60
         final.append(job_label)
     return final
-
-
 
 #import module
 from tkinter import *
@@ -82,43 +78,25 @@ root.configure(background ='paleturquoise')
 salary_label = Label(root,text= 'Pre-Tax Weekly Salary Calculator', bg='orchid',justify=CENTER).pack()
 
 create_Job_label()
-# job1 = Label(root, text= "1st Job", bg='paleturquoise').place(x = 140, y = 40)
-# job2 = Label(root, text= "2nd Job", bg='paleturquoise').place(x = 200, y = 40)
-# job3 = Label(root, text= "3rd Job", bg='paleturquoise').place(x = 260, y = 40)
 
 hours_worked = Label(root, text = "Hours Worked").place(x = 40,y = 70)
 create_Hour_input()
-# hours_worked_input = Entry(root, width=10)
-# hours_worked_input.place(x=130,y=70)
-# hours_worked_input2 = Entry(root, width=10)
-# hours_worked_input2.place(x=190,y=70)
-# hours_worked_input3 = Entry(root, width=10)
-# hours_worked_input3.place(x=250,y=70)
-
 
 wage = Label(root, text = "Wage per hour").place(x = 40,y = 110)
 create_Wage_input()
-# wage_input = Entry(root, width=10)
-# wage_input.place(x=130,y=110)
-# wage_input2 = Entry(root, width=10)
-# wage_input2.place(x=190,y=110)
-# wage_input3 = Entry(root, width=10)
-# wage_input3.place(x=250,y=110)
-
 
 bi_weekly_wage = Label(root, text="Bi-weekly Salary").place(x = 40, y = 150)
 total = Label(root, text="Sum of Salaries").place(x = 40, y = 180)
 
-calc_button = Button(root, text= "Calculate", bg='orchid', justify=CENTER, command = calculate).place(x =170,y=220)
-calc_button2 = Button(root, text= "Calculate", bg='orchid', justify=CENTER, command = calculate).place(x =170,y=220)
-calc_button3 = Button(root, text= "Calculate", bg='orchid', justify=CENTER, command = calculate).place(x =170,y=220)
-total_Sal = Button(root, text= "Calculate", bg='orchid', justify=CENTER, command = calculate).place(x =170,y=220)
+for i in range(3):
+    calc_button = Button(root, text= "Calculate", bg='orchid', justify=CENTER, command = calculate).place(x =170,y=220)
 
 clear = Button(root, text= "Clear", bg='orchid', justify=CENTER, command = clear).place(x =230,y=220)
 
 label = Label(root, text = "Insert Zeros if less than 3 jobs").place(x = 140,y = 250)
 
 root.mainloop()
+
 
 
 
